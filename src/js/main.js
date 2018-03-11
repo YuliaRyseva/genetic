ba94693357c8aeaf4337d3e2919318f9f4e73f84
YuliaRyseva <y.ryseva@yandex.ru>
2018-03-11 20:45:31 +0300

    initialize

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+//= ../libs/jquery/jquery-1.12.2.min.js
+//= ../libs/bootstrap/js/bootstrap-3.3.7.min.js
+//= ../libs/owl.carousel/js/owl.carousel-2.2.1.min.js
+//= ../libs/owl.carousel/js/owl.carousel.handler.js
